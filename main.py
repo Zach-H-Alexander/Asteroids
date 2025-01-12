9e5a16a6c29fb94c2350fa0995f146530b8f3ab5
Dixie <ironspud@gmail.com>
2025-01-12 14:42:28 -0500

    Ship moves!

--- main.py
+++ main.py
@@ -1,22 +1,30 @@
 
-#Import Pygame and Constants.py
+#Import Pygame and Constants.py and Player.py
 import pygame
 import constants
+from player import Player
 #Begin Game
 
 def main():
-	pygame.init()
-	screen = pygame.display.set_mode((constants.SCREEN_WIDTH, constants.SCREEN_HEIGHT))
-	print("Starting asteroids!")
-	print(f"Screen width: {constants.SCREEN_WIDTH}")
-	print(f"Screen height: {constants.SCREEN_HEIGHT}")
-	while True:
-		for event in pygame.event.get():
-			if event.type == pygame.QUIT:
-       				 return
+    pygame.init()
+    player  = Player(constants.SCREEN_WIDTH/2, constants.SCREEN_HEIGHT/2)
+    game_clock = pygame.time.Clock()
+    dt = 0
+    screen = pygame.display.set_mode((constants.SCREEN_WIDTH, constants.SCREEN_HEIGHT))
+    print("Starting asteroids!")
+    print(f"Screen width: {constants.SCREEN_WIDTH}")
+    print(f"Screen height: {constants.SCREEN_HEIGHT}")
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
 
-		screen.fill((0,0,0))
-		pygame.display.flip()
+        screen.fill((0,0,0))
+        player.update(dt)
+        player.draw(screen)
+        pygame.display.flip()
+        dt = game_clock.tick(60)/1000
+
 if __name__ == "__main__":
 	main()
 
